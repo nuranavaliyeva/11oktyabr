b5de29769e62be232f9754cd79318b3364d7b04a
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2024-10-11 11:24:40 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+
+namespace _11oktyabr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrey = { 1, 2, 3 };
+            CustomResize(ref arrey, 4, 5, 6);
+            for (int i = 0; i < arrey.Length; i++)
+            {
+                Console.WriteLine(arrey[i]);
+            }
+           
+            
+        }
+
+        static void CustomResize(ref int[] arr, params int[] arr1)
+        {
+            int[] newArr = new int[arr.Length + arr1.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                newArr[arr.Length + i] = arr1[i];
+            }
+            arr= newArr;
+            }
+            
+        }
+    }
+
+       
+        
